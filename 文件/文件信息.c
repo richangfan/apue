bfa67ec4badd2a0d859e1fe6caf24333a37c56f1
richangfan <richangfan@outlook.com>
2021-04-16 15:35:21 +0800

    fix

--- 文件/文件信息.c
+++ 文件/文件信息.c
@@ -1,8 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "../common.h"
 #include <errno.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("未输入文件路径\n");
+        error_exit();
+    }
+    int fd = open(argv[1], O_EXCL);
+    if (fd < 0)
+        printf("打开文件失败\n"), error_exit();
+    struct stat buf;
+    if (fstat(fd, &buf) != 0)
+        printf("fstat\n"), error_exit();
+    printf("所属用户是%d\n", buf.st_uid);
+    printf("所属用户组是%d\n", buf.st_gid);
+    printf("文件体积是%ld字节\n", buf.st_size);
+    printf("访问时间是%ld\n", buf.st_atim.tv_sec);
+    printf("修改时间是%ld\n", buf.st_mtim.tv_sec);
+    printf("状态更改时间是%ld\n", buf.st_ctim.tv_sec);
+    printf("硬链接数是%ld\n", buf.st_nlink);
+    printf("八进制文件权限是%o\n", buf.st_mode);
+    printf("%ld\n", buf.st_blksize);
+    printf("%ld\n", buf.st_dev);
+    printf("%ld\n", buf.st_rdev);
+    printf("%ld\n", buf.st_ino);
+    printf("%ld\n", buf.st_mtim.tv_nsec);
+    printf("%ld\n", buf.st_ctim.tv_nsec);
+    printf("%ld\n", buf.st_atim.tv_nsec);
+    exit(0);
+}
 
 void error_exit()
 {
@@ -55,35 +85,4 @@ void error_exit()
         break;
     }
     exit(1);
-}
-
-int main(int argc, char *argv[])
-{
-    if (argc != 2)
-    {
-        printf("未输入文件路径\n");
-        error_exit();
-    }
-    int fd = open(argv[1], O_EXCL);
-    if (fd < 0)
-        printf("打开文件失败\n"), error_exit();
-    struct stat buf;
-    if (fstat(fd, &buf) != 0)
-        printf("fstat\n"), error_exit();
-    printf("所属用户是%d\n", buf.st_uid);
-    printf("所属用户组是%d\n", buf.st_gid);
-    printf("文件体积是%ld字节\n", buf.st_size);
-    printf("访问时间是%ld\n", buf.st_atim.tv_sec);
-    printf("修改时间是%ld\n", buf.st_mtim.tv_sec);
-    printf("状态更改时间是%ld\n", buf.st_ctim.tv_sec);
-    printf("硬链接数是%ld\n", buf.st_nlink);
-    printf("八进制文件权限是%o\n", buf.st_mode);
-    printf("%ld\n", buf.st_blksize);
-    printf("%ld\n", buf.st_dev);
-    printf("%ld\n", buf.st_rdev);
-    printf("%ld\n", buf.st_ino);
-    printf("%ld\n", buf.st_mtim.tv_nsec);
-    printf("%ld\n", buf.st_ctim.tv_nsec);
-    printf("%ld\n", buf.st_atim.tv_nsec);
-    exit(0);
 }
