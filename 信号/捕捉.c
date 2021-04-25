6c0ef5379d73e9e355cc97a44f2a678f34d5c88c
richangfan <richangfan@outlook.com>
2021-04-26 00:11:53 +0800

    捕捉信号

---
+++ 信号/捕捉.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+void error_exit(char *msg)
+{
+    printf("%s\n", msg);
+    exit(1);
+}
+
+void handle(int sig)
+{
+    if (sig == SIGINT)
+        printf("捕捉到了INT信号\n");
+    else
+        printf("捕捉到了未知信号\n");
+    exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+    if (signal(SIGINT, handle) == SIG_ERR)
+        error_exit("设置INT信号处理程序出错\n");
+    while (1)
+    {
+        printf("\n");
+    }
+    exit(0);
+}
