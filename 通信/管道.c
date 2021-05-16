8f4ca48cfbc517ba6aacc9dfd0c10aa468db8165
richangfan <richangfan@outlook.com>
2021-05-16 21:57:22 +0800

    fix

--- 通信/管道.c
+++ 通信/管道.c
@@ -11,8 +11,13 @@ void error_exit(char *msg)
 int main(int argc, char *argv[])
 {
     int fd[2];
+    const char *s = "hello";
+    char buf[1024];
 
     if (pipe(fd) != 0)
         error_exit("创建pipe出错");
+    write(fd[1], s, sizeof(s));
+    read(fd[0], &buf, sizeof(s));
+    printf("%s\n", buf);
     exit(0);
 }
